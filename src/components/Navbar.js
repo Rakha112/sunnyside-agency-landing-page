e80d5885e0c0e23a4c8e2709341d07366d6763e0
Rakha <rakhawibowo1998@gmail.com>
2021-06-24 15:43:32 +0700

    update

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,18 +6,20 @@ const Navbar = () => {
     const [isAktif,setisAktif] = useState(false)
 
     return (
-        <div className="navbar">
-            
-            <img src={logo} alt="" />
-            <ul className={isAktif ? "aktif" : ""}>
-                <div id="arrow-left"></div>
-                <li><a href="#about">About</a></li>
-                <li><a href="#services">Services</a></li>
-                <li><a href="#projects">Projects</a></li>
-                <li className="li_con"><a href="#contact" className="contact">Contact</a></li>
-            </ul>
-            <img onClick={() => setisAktif(!isAktif)} src={ham} alt="" />
-        </div>
+            <div className="navbar">
+
+                <div className="wrapper">
+                    <img src={logo} alt="" />
+                    <ul className={isAktif ? "aktif" : ""}>
+                        <div id="arrow-left"></div>
+                        <li><a href="#about">About</a></li>
+                        <li><a href="#services">Services</a></li>
+                        <li><a href="#projects">Projects</a></li>
+                        <li className="li_con"><a href="#contact" className="contact">Contact</a></li>
+                    </ul>
+                    <img onClick={() => setisAktif(!isAktif)} src={ham} alt="" />
+                </div>   
+            </div>
     )
 }
 
